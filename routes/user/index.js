1f59894fbbfa8141212e6e72a78eb549b9178177
daryldixon1994 <kmohamedanouar@gmail.com>
2024-01-06 18:26:21 +0100

    add p

--- routes/user/index.js
+++ routes/user/index.js
@@ -23,8 +23,9 @@ route.post("/register", async (req, res) => {
 
 route.put("/add-photo/:id", upload.single("photo"), async (req, res) => {
   try {
+    console.log("req.protocol: ", req.protocol);
     let { id } = req.params;
-    let userImg = `${req.protocol}://${req.get("host")}/uploads/${
+    let userImg = `${req.protocol}s://${req.get("host")}/uploads/${
       req.file.filename
     }`;
     let newUser = await User.findByIdAndUpdate(
